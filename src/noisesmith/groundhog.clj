8a53dbd16e70487e33635ce4f1e8c8647efcb8d5
Justin Smith <noisesmith@gmail.com>
2013-11-07 11:13:03 -0800

    refactoring, handling of defaults

--- src/noisesmith/groundhog.clj
+++ src/noisesmith/groundhog.clj
@@ -6,35 +6,37 @@
 
 (defonce requests (atom []))
 
-(defn tee-stream
-  [stream]
-  (let [buffer (ByteArrayOutputStream.)
-        _ (io/copy stream buffer)
-        bytes (.toByteArray buffer)]
-    {:stream (ByteArrayInputStream. bytes)
-     :contents bytes}))
-
 (defn serialize
+  "Make a version of the debug-request that can be transperently printed and
+   read."
   [debug-request]
   (dissoc debug-request :body))
 
-(defn debugging
-  "Sanitizer will get a byte-array and should return one, and has the opportunity
-   to remove any data that may be considered sensitive before storage. We use
-   byte array because the input may not be a string (though it usually will be)."
-  [handler sanitizer]
-  (fn [request]
-    (let [{body :stream
-           contents :contents} (tee-stream (:body request))
-          encoded-bytes (-> contents
-                      sanitizer
-                      b64/encode)
-          encoded (String. ^bytes encoded-bytes)
-          backed-up-request (assoc request
-                              :body body
-                              :body64 encoded)]
-      (swap! requests conj (serialize backed-up-request))
-      (handler backed-up-request))))
+(defn default-sanitize
+  "The non-sanitizing sanitize"
+  [base64-byte-array]
+  base64-byte-array)
+
+(defn transform-as-string
+  [base64-byte-array f]
+  (-> base64-byte-array
+      (#(String. ^bytes %))
+      f
+      (#(.getBytes ^String %))))
+
+(defn default-store
+  [serialized-request]
+  (swap! requests conj serialized-request))
+
+(defn default-replay
+  [request]
+  (let [{:keys [uri remote-addr]} request
+        route-re #"/replay/([0-9])+"
+        [_ match-num-str] (re-matches route-re uri)]
+    (when (and (= remote-addr "127.0.0.1")
+               match-num-str)
+      (nth @requests (read-string match-num-str)))))
+
 
 (defn deserialize
   [serialized-request]
@@ -44,7 +46,56 @@
         stream (ByteArrayInputStream. decoded)]
     (assoc serialized-request :body stream)))
 
-(defn replayer
-  [handler sanitizer]
-  (fn [serialized-request]
-    (handler (deserialize serialized-request))))
+(defn tee-stream
+  [stream]
+  (let [buffer (ByteArrayOutputStream.)
+        _ (io/copy stream buffer)
+        bytes (.toByteArray buffer)]
+    {:stream (ByteArrayInputStream. bytes)
+     :contents bytes}))
+
+(defn duplicate
+  "Duplicates the request and stores a serialized version."
+  [request sanitize store]
+  (let [{body :stream
+         contents :contents} (tee-stream (:body request))
+         encoded-bytes (-> contents
+                           sanitize
+                           b64/encode)
+         encoded (String. ^bytes encoded-bytes)
+         duplicated-request (assoc request
+                              :body body
+                              :body64 encoded)]
+    (store (serialize duplicated-request))
+    duplicated-request))
+
+(defn groundhog
+  "A ring middleware to record and then retrieve and optionally replay requests.
+   Opts:
+   sanitize
+            will get a byte-array and should return one, and has the opportunity
+       to remove any data that may be considered sensitive before storage. We
+       use byte array because the input may not be a string (though it usually
+       will be).
+            defaults to identity
+   store
+            will get the serialized form of the request as an argument
+            defaults to conjing on a global atom in this namespace.
+   replay
+            a predicate on the request, if it returns a serialized request, that
+       request will be replayed
+            defaults to looking for /replay/<count> and retrieving from the
+       request atom in this namespace"
+  [handler & [{:keys [sanitize store replay]
+               :or {sanitize default-sanitize
+                    store default-store
+                    replay default-replay}
+               :as opts}]]
+  (fn [request]
+    (if-let [stored (replay request)]
+      ;; if replay returns truthy, we do not store the request, and we replay
+      ;; the data that it hands back to us
+      (handler (deserialize stored))
+      ;; if replay returns falsey, we store the sanitized request for later,
+      ;; and then pass the non-sanitized on to the handler as normal
+      (handler (duplicate request sanitize store)))))
