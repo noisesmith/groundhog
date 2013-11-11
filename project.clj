852df01634879eebed30f1570e72c920b2e1831b
Justin Smith <noisesmith@gmail.com>
2013-11-10 20:06:47 -0800

    adding a licencse in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject noisesmith/groundhog "0.0.3"
   :description "A clojure ring request recorder and replayer."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.codec "0.1.0"]]
   :source-paths ["src"])
